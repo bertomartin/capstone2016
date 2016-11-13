8e3f67b662d6f8020f8fe6b5a00120398eb1a47b
Roberto <rmartin.martin@gmail.com>
2016-11-12 22:18:01 -0500

    adding code to move data from csv to mongo

--- src/io.py
+++ src/io.py
@@ -3,17 +3,5 @@ This file is used for input/output of data from different stores: s3, mongo, csv
 """
 
 
-def s3_store_data():
-    pass
-
-
-def s3_get_data():
-    pass
-
-
-def mongo_get_data(conn, db):
-    pass
-
-
-def mongo_store_data(conn, db):
+def csv_to_mongo():
     pass
